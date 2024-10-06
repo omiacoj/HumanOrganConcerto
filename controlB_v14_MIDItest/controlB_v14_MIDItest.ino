a1357102120c155835c3983e092000984c2e1c47
Sjoerd Leijten <sjo.leijten@gmail.com>
2024-10-06 23:45:11 +0200

    update

---
+++ controlB_v14_MIDItest/controlB_v14_MIDItest.ino
@@ -0,0 +1,115 @@
+//#define DEBUG
+#define PUBLISH_DELTA 10
+//variables
+
+bool isCapturingA = false;
+bool isCapturingB = false;
+
+byte indexB = 0;
+uint8_t indexA = 0;
+
+// array to hold xbee frame 0x92 excluding header byte
+char inDataA[22];
+char inDataB[22];
+unsigned long AD1; 
+unsigned long AD2;
+unsigned long noses[4];  // array to hold 13 values for nose information
+unsigned long store_noses[4];
+int calib_noses[4];
+int calib_nose_times[4];
+int nose_switches[4];
+
+int switchOff1 = 0;
+int switchOff2 = 0;
+
+
+unsigned long mouths[4];  // array to hold 13 values for mouth information
+unsigned long store_mouths[4];
+//unsigned long calib_mouths[13]; 
+int calib_mouths[4];
+int calib_mouth_times[4];
+int mouth_switches[4];
+
+//int controlA[5][4] = { {24, 25, 2, 3}, {4, 5, 6, 7}, {8, 9, 10, 11}, {12, 13, 18, 19}, {20, 21, 22, 23} };
+int controlA[4][4] = { {61,62,63,64},{65,66,67,68},{69,70,71,72},{73,74,75,76} };
+
+
+int scramble_check[4][4] = { {0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0} };
+
+//int list[] = { 24, 25, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 18, 19, 20, 21, 22, 23 };
+// nieuwe miditonenlijst
+int list[] = { 61,62,63,64,65,66,67,68,69,70,71,72,77,74,79,81 };
+//61 63 68 70
+
+const size_t n = sizeof(list) / sizeof(list[0]);
+
+
+//int controlB[5][4] = { {24,25,26,27},{28,29,30,31},{32,33,34,35},{36,37,38,39},{40,41,42,43} };
+long lastDebounceTime = 0;
+long timeline = 0;
+long timestructure = 4;
+int timeswitch = 0;
+
+long maxi = 525;
+long mini = 475;
+
+unsigned long publishTime = 0;
+
+/*for the MIDI stuff */
+ int velocity = 100;//velocity of MIDI notes, must be between 0 and 127
+ int noteON = 144; //144 = 10010000 in binary, note on command
+ int noteOFF = 128; //128 = 10000000 in binary, note off command
+
+
+
+void setup() {
+  // start serial
+  Serial2.begin(9600);
+  Serial.begin(31250);
+  //Serial1.begin;
+  //Serial2.begin;
+
+  Serial3.begin(9600);
+
+  //publishTime = millis() + PUBLISH_DELTA;
+}
+
+void loop() {
+  //unsigned long delta = millis();
+  // publish values after PUBLISH_DELTA ms have passed
+
+
+  for (int i = 0; i < 16; i++)
+  {
+    MIDImessage(noteON, list[i], velocity);
+    //MIDImessage(noteON, i, velocity);
+    Serial.println(i);
+    delay(1000);
+    MIDImessage(noteOFF, list[i], velocity);
+    //MIDImessage(noteOFF, i, velocity);
+    delay(500);
+  }
+  /*
+       while(Serial2.available() > 0 )
+    {
+       Serial.print("Serial 2 available");
+      //char inCharA = Serial3.read();
+    }
+  */
+
+   // end serial whilex
+
+
+
+
+  //actionA();
+
+
+  
+}
+
+void MIDImessage(int command, int MIDInote, int MIDIvelocity) {
+  Serial.write(command);//send note on or note off command 
+  Serial.write(MIDInote);//send pitch data
+  Serial.write(MIDIvelocity);//send velocity data
+}
---
+++ controlB_v15_newMIDItones/Bparser.ino
@@ -0,0 +1,48 @@
+
+/*
+void parseSerialB()
+{
+  uint8_t length_MSB  = inDataB[0];  // typical 0x00
+  uint8_t length_LSB  = inDataB[1];  // length of frame
+  uint8_t frame       = inDataB[2];  // typical 0x92 data sample
+  uint8_t SH1         = inDataB[3];  // typical 0x00 xbee address MSB
+  uint8_t SH2         = inDataB[4];  // typical 0x13 xbee address
+  uint8_t SH3         = inDataB[5];  // typical 0xA2 xbee address
+  uint8_t SH4         = inDataB[6];  // typical 0x00 xbee address LSB
+  uint8_t SL1         = inDataB[7];  // source xbee address MSB
+  uint8_t SL2         = inDataB[8];  // source xbee address
+  uint8_t SL3         = inDataB[9];  // source xbee address
+  uint8_t SL4         = inDataB[10]; // source xbee address LSB
+  uint8_t MY1         = inDataB[11]; // source network address MSB
+  uint8_t MY2         = inDataB[12]; // source network address LSB
+  uint8_t recv_opt    = inDataB[13]; // receive options
+  uint8_t samples     = inDataB[14]; // samplesets - defaults always to 01
+  uint8_t D_mask1     = inDataB[15]; // digital channel mask MSB
+  uint8_t D_mask2     = inDataB[16]; // digital channel mask LSB
+  uint8_t A_mask      = inDataB[17]; // analog channel mask
+  uint8_t AD1_MSB     = inDataB[18]; // data sample MSB from AD1
+  uint8_t AD1_LSB     = inDataB[19]; // data sample LSB from AD1
+  uint8_t AD2_MSB     = inDataB[20]; // data sample MSB from AD2
+  uint8_t AD2_LSB     = inDataB[21]; // data sample LSB from AD2
+  
+  // assigning sample values to corresponding sources in arrays 'noses' and 'mouths' 
+  AD1 = AD1_LSB + (AD1_MSB * 256); 
+  AD2 = AD2_LSB + (AD2_MSB * 256);
+  // goeie
+  if (SL1 == 0x40 && SL2 == 0xAD && SL3 == 0xD0 && SL4 == 0x91)
+    {
+      noses[3] = AD1;
+      mouths[3] = AD2;
+    }
+  // werkt niet
+  if (SL1 == 0x40 && SL2 == 0xAB && SL3 == 0xB9 && SL4 == 0x48)
+    {
+      noses[4] = AD1;
+      mouths[4] = AD2;
+    }
+ 
+//#ifdef DEBUG
+//#endif
+}
+
+*/
